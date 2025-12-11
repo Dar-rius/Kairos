b6402f712fcc41f5f2029213b32a2c458e5ae5db
Dar-rius <mohamedtine17@gmail.com>
2025-12-11 14:16:24 +0000

    changer some functions

--- rl_trade/compute.py
+++ rl_trade/compute.py
@@ -1,6 +1,22 @@
 import pandas as pd
 import numpy as np
 from scipy.stats import entropy
+import math
+
+BETA = 1
+
+def reward(return_: float, cost_rate: float, action: int, prob: list[float], entropy_low: float = .3) -> float:
+    # Compute the micro strategy for trading 1h
+    first_micro = return_ * action 
+    second_micro = cost_rate * abs(action)
+    micro_strat = first_micro - second_micro
+    
+    # Compute the macro strategy for detect the state of market
+    first_macro = BETA * abs(action)
+    second_macro = max(0, compute_entropy(prob) - entropy_low)
+    macro_strat = first_macro * second_macro
+
+    return micro_strat - macro_strat
 
 #Compute the sharpe ration
 def calcul_sharpe_ratio(data_p: list[float], data_btc: list[float]) -> float:
@@ -9,8 +25,8 @@ def calcul_sharpe_ratio(data_p: list[float], data_btc: list[float]) -> float:
     n = len(data_p)
 
     #compute the return of portfolio
-    portfolio_return = [pct_change(data_p[i], data_p[i-1]) for i in range(1, n)]
-    btc_return = [pct_change(data_btc[i], data_btc[i-1]) for i in range(1, n)]
+    portfolio_return = [return_log(data_p[i], data_p[i-1]) for i in range(1, n)]
+    btc_return = [return_log(data_btc[i], data_btc[i-1]) for i in range(1, n)]
 
     #compute the return excess, mean and the derivating
     excess = [p - btc for p, btc in zip(portfolio_return, btc_return)]
@@ -21,19 +37,13 @@ def calcul_sharpe_ratio(data_p: list[float], data_btc: list[float]) -> float:
     sr = excess_avg/excess_std
     if np.isnan(sr):
         return 0.0
-
     return sr.item()
 
-def compute_belief(data_p: list[float], cost: float, prob: list[float], beta: float) -> float:
-    if len(data_p) < 2:
-        return 0.0
-    n = len(data_p)
+def compute_cost(amount: float, cost_rate: float): return amount * cost_rate
 
-    portfolio_return = pct_change(data_p[n-1], data_p[n-2])
-    entropy_prob = entropy(prob, base=2)
-    return portfolio_return - cost - (beta * entropy_prob)
+def compute_entropy(prob: list[float]): return entropy(prob, base=2)
 
 def calcul_total_profit(precedent_tp: float, now_tp: float): return precedent_tp - now_tp
 
-#Compute the return
-def pct_change(current: float, previous: float): return (current - previous) / previous
+#Compute the return log
+def return_log(current: float, previous: float): return math.log(current / previous)
--- rl_trade/env.py
+++ rl_trade/env.py
@@ -1,42 +1,50 @@
 import pandas as pd
-from .compute import calcul_sharpe_ratio, calcul_total_profit
+from .compute import return_log, calcul_sharpe_ratio, calcul_total_profit, reward
 from .processing import df_to_list, concat_df, convert_tensor_to_list, convert_to_btc, convert_to_usd
 from collections import namedtuple, deque
 
 # Create a group sequence
 class SequenceGroup:
-    def __init__(self, memory_size:int):
+    def __init__(self, memory_size: int):
         self.memory = deque([], maxlen=memory_size)
-        self.sequence = namedtuple("Sequence", ('open', 'high', 'low', 'close', 'volume'))
+        #self.micro_day = namedtuple("MicroDay", ('open', 'high', 'low', 'close', 'volume'))
+        #self.micro = namedtuple("Micro", ('data'))
+        #self.macro = namedtuple("Macro", ('open', 'high', 'low', 'close', 'volume'))
 
-    def push(self, *args):
-        self.memory.append(self.sequence(*args))
+    #def push_macro(self, *args): self.macro(*args)
+
+    #def push_micro(self, *args): self.micro(self.micro_day(*args))
+
+    def push(self, data: list(list)):
+        self.memory.append(data)
         
     def clear(self):
         self.memory.clear()
 
-    def sample(self) -> list:
-        return self.memory 
+    def sample(self): return self.memory 
 
-    def len(self) -> int:
-        return len(self.memory)
+    #Show the size of memory
+    def len(self): return len(self.memory)
 
 # ******* ENV **********
 
 class Env:
-    def __init__(self, data: pd.DataFrame, amount_usd: int = 100000.0, memory_size: int = 5):
+    def __init__(self, daily_trade: pd.DataFrame, macro_trade: pd.DataFrame, memory_size: int, amount_usd: int = 100000.0, cost_rate: float = 0.001):
         self.portfolio_values: list[float] = []
         self.btc_values: list[float] = []
-        self.MEMORY_SIZE = memory_size
+        #self.memory_size = memory_size
         self.historic_data = pd.DataFrame(data = {'action':list[int], 'portfolio': list[float]})
         self.reward: float = 0
-        self.data: pd.DataFrame = data
+        self.daily_trade = daily_trade
+        self.macro_trade = macro_trade
         self.metric = pd.DataFrame(data={'date':[], "sharpe ratio": [], "tp": []})
         self.total_amount: dict = {0: amount_usd, 1: 0}
-        self.first_minute = 0
-        self.last_minute = memory_size
+        #self.first_minute = 0
+        #self.last_minute = memory_size
         self.sequence_group = SequenceGroup(memory_size)
         self.btc_value: float = 0.0
+        self.cost_rate = cost_rate
+        self.seq: int = 0
 
     def __buy(self):
         print(self.btc_value)
@@ -54,34 +62,29 @@ class Env:
             return convert_to_usd(self.total_amount[1], self.btc_value)
 
     #Create a group state
-    def create_batch(self) -> tuple[list, bool]: 
-        #Clear the previous state
-        if self.sequence_group.len() > 0:
-            self.sequence_group.clear()
-
-        done = False
-        dataset = self.data.loc[self.first_minute : self.last_minute]
-        values = df_to_list(dataset)
-        self.sequence_group.push(values[0], values[1], values[2], values[3], values[4])
-        print(values[3])
-        self.btc_value = values[3][-1]
-        check_next_nb_time = len(self.data.loc[self.first_minute:, "Open"])
+    def create_batch(self, n_days:int = 85, n_minutes:int = 60) -> list: 
+        #dataset = self.data.loc[self.first_minute : self.last_minute]
+        #values = df_to_list(dataset)
+        daily_trades = [self.daily_trade[i:i + n_minutes] for i in range(0, self.daily_trade.shape[0], n_minutes)]
+        macro_trades = [self.macro_trade[i:i + n_days] for i in range(0, self.macro_trade.shape[0], n_days)]
+        self.sequence_group.push([daily_trades, macro_trades])
+        #self.btc_value = values[3][-1]
+        #check_next_nb_time = len(self.data.loc[self.first_minute:, "Open"])
         
         #check the rest quantity of data
-        self.first_minute = self.last_minute
-        if  check_next_nb_time > self.MEMORY_SIZE:
-            self.last_minute += self.MEMORY_SIZE
-        else:
-            self.last_minute += check_next_nb_time 
-            done = True
-
-        return (self.sequence_group.sample(), done)
+        #self.first_minute = self.last_minute
+        #if  check_next_nb_time > self.MEMORY_SIZE:
+            #self.last_minute += self.MEMORY_SIZE
+        #else:
+            #self.last_minute += check_next_nb_time 
+            #done = True
+        return  self.sequence_group.sample()[self.seq]
 
     #Reset the env to 0
     def reset(self): return self.create_batch()
 
     #The next step of env
-    def step(self, action: int) -> tuple:
+    def step(self, action: int, prob: list[float]) -> tuple:
         if action  == -1:
             trade_info = [action, self.calcul_portfolio_value()]
             self.historic_data = concat_df(self.historic_data,  trade_info)
@@ -97,6 +100,8 @@ class Env:
         #Compute the reward
         self.portfolio_values.append(self.calcul_portfolio_value())
         self.btc_values.append(self.btc_value)
-        reward: float = calcul_sharpe_ratio(self.portfolio_values, self.btc_values)
-        state, done = self.create_batch()
-        return (state, done, reward)
+        return_ = return_log(self.btc_values[-1], self.btc_values[-2])
+        reward: float = reward(return_, self.cost_rate, action, prob)
+        self.seq += 1
+        state = self.sequence_group.sample()[self.seq]
+        return (state, reward)
