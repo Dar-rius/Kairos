438e714cf846f36d2fd905c9e2739a09ef04ea95
Dar-rius <mohamedtine17@gmail.com>
2025-12-14 13:09:08 +0000

    update

--- rl_trade/env.py
+++ rl_trade/env.py
@@ -2,6 +2,8 @@ import pandas as pd
 from .compute import return_log, calcul_sharpe_ratio, calcul_total_profit, reward
 from .processing import df_to_list, concat_df, convert_tensor_to_list, convert_to_btc, convert_to_usd
 from collections import namedtuple, deque
+#import cudf as cu
+#import cupy as cp
 
 # Create a group sequence
 class SequenceGroup:
@@ -29,19 +31,17 @@ class SequenceGroup:
 # ******* ENV **********
 
 class Env:
-    def __init__(self, daily_trade: pd.DataFrame, macro_trade: pd.DataFrame, memory_size: int, amount_usd: int = 100000.0, cost_rate: float = 0.001):
+    def __init__(self, daily_trade: pd.DataFrame, macro_trade: pd.DataFrame, n_days:int, amount_usd: int = 100000.0, cost_rate: float = 0.001):
         self.portfolio_values: list[float] = []
         self.btc_values: list[float] = []
-        #self.memory_size = memory_size
         self.historic_data = pd.DataFrame(data = {'action':list[int], 'portfolio': list[float]})
         self.reward: float = 0
         self.daily_trade = daily_trade
         self.macro_trade = macro_trade
+        self.n_days = n_days
+        self.start_batch: int = 0
         self.metric = pd.DataFrame(data={'date':[], "sharpe ratio": [], "tp": []})
         self.total_amount: dict = {0: amount_usd, 1: 0}
-        #self.first_minute = 0
-        #self.last_minute = memory_size
-        self.sequence_group = SequenceGroup(memory_size)
         self.btc_value: float = 0.0
         self.cost_rate = cost_rate
         self.seq: int = 0
@@ -62,29 +62,14 @@ class Env:
             return convert_to_usd(self.total_amount[1], self.btc_value)
 
     #Create a group state
-    def create_batch(self, n_days:int = 9, n_minutes:int = 60) -> list: 
-        #dataset = self.data.loc[self.first_minute : self.last_minute]
-        #values = df_to_list(dataset)
-        daily_trades = []
-        for _ in range(0, n_days):
-            hour_trade = []
-            for _ in range(0,24):
-                hour_trade = [self.daily_trade[i:i + 60] for i in range(0, self.daily_trade.shape[0], 60)]
-            daily_trades.append(hour_trade)
-
-        macro_trades = [self.macro_trade[i:i + n_days] for i in range(0, self.macro_trade.shape[0], n_days)]
-        self.sequence_group.push([daily_trades, macro_trades])
-        #self.btc_value = values[3][-1]
-        #check_next_nb_time = len(self.data.loc[self.first_minute:, "Open"])
-        
-        #check the rest quantity of data
-        #self.first_minute = self.last_minute
-        #if  check_next_nb_time > self.MEMORY_SIZE:
-            #self.last_minute += self.MEMORY_SIZE
-        #else:
-            #self.last_minute += check_next_nb_time 
-            #done = True
-        return  self.sequence_group.sample()[self.seq]
+    def create_batch(self) -> tuple: 
+        days_trade = self.daily_trade.shape[0] // 1440
+        daily_trades = self.daily_trade[self.start_batch:self.n_days].to_numpy()
+        macro_days = self.macro_trade[self.start_batch:self.n_days].to_numpy()
+        self.start_batch += self.n_days
+        self.n_days += self.n_days
+        print(days_trade)
+        return (daily_trades, macro_days)
 
     #Reset the env to 0
     def reset(self): return self.create_batch()
@@ -103,7 +88,7 @@ class Env:
             trade_info = [action, self.calcul_portfolio_value()]
             self.historic_data = concat_df(self.historic_data,  trade_info)
 
-        state = self.sequence_group.sample()[self.seq]
+        state = self.create_batch()
         self.portfolio_values.append(self.calcul_portfolio_value())
         #the value [1] is just a test
         self.btc_values.append(state[0][1])
--- tests/test_env.py
+++ tests/test_env.py
@@ -2,39 +2,35 @@ import pandas as pd
 from rl_trade.env import Env, SequenceGroup, df_to_list 
 import unittest
 from pandas.testing import assert_frame_equal
+import numpy as np
+#import cudf as cu
+#import cupy as cp
 
 MEMORY_SIZE = 5
 df = pd.read_csv("./data_off/unit_test/unit_test_df_price.csv")
+jf = df.drop('Datetime_utc', axis=1)
 df_1 = pd.read_csv("./data_off/unit_test/unit_test_df_metric.csv")
+df_1 = df_1.drop(['date', 'state'], axis=1)
 sequence_group = SequenceGroup(MEMORY_SIZE)
 
 def test_reset():
-    env = Env(daily_trade=df, macro_trade=df_1, memory_size=85)
-    result = env.reset()
-    daily_trades = []
-    for _ in range(0, 9):
-        hour_trade = []
-        for _ in range(0,24):
-            hour_trade = [df[i:i + 60] for i in range(0, df.shape[0], 60)]
-        daily_trades.append(hour_trade)
-
-    macro_trades = [df_1.loc[i] for i in range(0, df_1.shape[0])]
-    sequence_group.push([daily_trades, macro_trades])
-    print("test: ", sequence_group.sample()[0][1][0])
-    assert len(result[0][0]) == len(sequence_group.sample()[0][0][0])
-    for df_res, df_att in zip(result[0][0], sequence_group.sample()[0][0][0]):
-        assert_frame_equal(df_res, df_att)
-    for df_res, df_att in zip(result[1][1], sequence_group.sample()[0][1][1]):
-        assert_frame_equal(df_res, df_att)
+    env = Env(daily_trade=df, macro_trade=df_1, n_days=10)
+    tab1, tab2 = env.reset()
+    daily_trades = df[0:10].to_numpy()
+    macro_trades =  df_1[0:10].to_numpy()
+    np.testing.assert_equal(tab1, daily_trades)
+    np.testing.assert_equal(tab2, macro_trades)
+    
 
 def test_calcul_portfolio_value():
-    env = Env(daily_trade=df, macro_trade=df_1, memory_size=85)
+    env = Env(daily_trade=df, macro_trade=df_1, n_days=85)
     result = env.calcul_portfolio_value()
     assert result == 100000.0
 
+"""
 # Test all case of action
 class TestStep:
-    env = Env(daily_trade=df, macro_trade=df_1, memory_size=85)
+    env = Env(daily_trade=df, macro_trade=df_1, n_days=85)
     env.reset()
     seq = SequenceGroup(MEMORY_SIZE)
 
@@ -60,3 +56,4 @@ class TestStep:
         self.seq.clear()
         self.seq.push(values[0], values[1], values[2], values[3], values[4])
         assert result  ==  (self.seq.sample(), False, 0.0)
+"""
